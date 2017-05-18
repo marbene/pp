f82a5f8213e7d138ad40015d192d9b8c0e4f19d6
Maira Simonetti <Simai_93@hotmail.it>
2017-05-18 19:18:39 +0200

    Add files via upload

---
+++ EUtenteR.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Maira Simonetti
+ * Date: 18/05/17
+ * Time: 18:20
+ */
+class EUtenteR extends EPersona
+{ private $Username;
+  private $password;
+  private $feedback;
+  private $auto;
+
+
+  public function __contruct() {}
+  public function __construct($) {}
+
+}
